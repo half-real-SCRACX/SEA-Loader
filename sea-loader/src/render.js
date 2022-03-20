ad1155465500646794e4a0eec16c7b05437ef4d0
SCRACX <robertlupas09@gmail.com>
2022-03-20 21:07:08 +0200

    Update render.js

--- sea-loader/src/render.js
+++ sea-loader/src/render.js
@@ -1,11 +1,44 @@
-const retryIC = document.getElementById('retryICbtn');
-retryIC.onclick = OnlineReCheck;
+const loadBtn = document.getElementById('loadBtn');
+loadBtn.onclick = loadPre;
 
 const { remote } = require('electron');
 
-async function OnlineReCheck(){
+// needed vars
+let isDir = 0;
+let isOnlineVar = 0;
+
+// do this when loadBtn is pressed
+function loadPre(){
+    checkdir()
+    OnlineCheck()
+}
+
+//---------------------------------------
+// functions that loadPre() uses
+
+// is user online?
+async function OnlineCheck(){
     const isOnline = require('is-online');
     (async () => {
-        console.log(await isOnline());
+        if(await isOnline() == true){
+        isOnlineVar = 1;
+        } else {isOnlineVar = 0;}
+        console.log(isOnlineVar);
     })();
+}
+
+
+// checkdir
+function checkdir(){
+const fs = require('fs');
+var path = require('path');
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+
+const dir = 'C:/Users/'+userName+'/AppData/LocalLow/Oskar Stalberg/Townscaper/Textures';
+
+if (fs.existsSync(dir)) {
+    console.log('Directory exists!');
+} else {
+    console.log('Directory not found.');
+}
 }

2cffee2f90acc9059eaccc40b07159450a72bd1c
SCRACX <robertlupas09@gmail.com>
2022-03-20 18:35:51 +0200

    Update index.js

--- sea-loader/src/index.js
+++ sea-loader/src/index.js
@@ -1,24 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
-}
-
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 750,
     height: 750,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   });
-
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 app.on('ready', createWindow);

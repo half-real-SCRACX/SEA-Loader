5a6904fcfa6b0dde295f33e6f3df9fa281086375
SCRACX <robertlupas09@gmail.com>
2022-03-21 21:53:14 +0200

    implementing function that checks available textures from the GitHub repo

--- sea-loader/src/TextureLoader.js
+++ sea-loader/src/TextureLoader.js
@@ -1,3 +1,32 @@
 const loadBtn = document.getElementById('loadBtn');
 
 const { remote } = require('electron');
+
+
+// get available textures from GitHub
+
+function getInfo(){
+    var data = [
+        {
+            "id":"1",
+            "Title":"1",
+            "description":"lorem ipsum",
+            "URL":"example.com/1"
+        },
+        {
+            "id":"2",
+            "Title":"2",
+            "description":"lorem ipsum",
+            "URL":"example.com/2"
+        }
+        ]
+    var root = document.getElementById('container');
+
+    for( var key in data ){
+    element = '<div> Title: ' + data[key].Title +
+        ', id: ' + data[key].id +
+        ', description: '+ data[key].description+
+        '</div>';
+    root.innerHTML += element;
+    }
+}

4b9c9b6c8777cfca30d3718879b6a88676904c56
SCRACX <robertlupas09@gmail.com>
2022-03-23 19:52:14 +0200

    implementing download function...

--- sea-loader/src/TextureLoader.js
+++ sea-loader/src/TextureLoader.js
@@ -24,11 +24,23 @@ function getInfo(){
     element = '<div> Title: ' + item.Title +
         ', id: ' + item.id +
         ', description: '+ item.description+
-        '<button onclick="downloadRequestedTexture(' + item.id + ')">' + item.Title + '</>'
+        '<button onclick="downloadRequestedTexture(' + item.URL + ')">' + item.Title + '</>'
         '</div>';
     root.innerHTML += element;
     }
 }
 
 // download the texture
-function downloadRequestedTexture(fileToDownload)
+function downloadRequestedTexture(fileToDownload){
+// find needed path
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+console.log(userName);
+
+const fs = require('fs');
+const dir = 'C:/Users/'+userName+'/AppData/LocalLow/Oskar Stalberg/Townscaper/Textures';
+
+// actually download the file
+var exactURL1 = fileToDownload + ;
+const { DownloaderHelper } = require('node-downloader-helper');
+const dl = new DownloaderHelper(, __dirname);
+}
